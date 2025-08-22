1563fa244a5fe86f6cb1f064d212f64b2bc412f4
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-08-22 16:52:23 +0700

    add responsive ui for app

---
+++ tester/lib/src/theme/app_responsive_ui.dart
@@ -0,0 +1,85 @@
+import 'dart:ui' as ui;
+
+import 'package:flutter/material.dart';
+
+class ScreenUtil {
+  bool? _isSmallScreen;
+  bool _isTablet = false;
+  double _screenWidth = 0;
+  double _screenHeight = 0;
+  double _screenRatioToDesign = 1;
+  double _screenRatioHeightToDesign = 1;
+  double _screenSemiRatioToDesign = 1;
+  double _sizeScale = 1;
+
+  static final ScreenUtil _instance = ScreenUtil._();
+
+  ScreenUtil._();
+
+  factory ScreenUtil() {
+    return _instance;
+  }
+
+  void init(BuildContext context) {
+    // if (_screenWidth != 0) return;
+    _screenWidth = MediaQuery.of(context).size.width;
+    _screenHeight = MediaQuery.of(context).size.height;
+    _isSmallScreen = _detectSmallScreen();
+    _isTablet = _detectTablet();
+
+    /// Dependent
+    _sizeScale = _isSmallScreen! ? 0.85 : 1.0;
+    _screenRatioToDesign = _screenWidth / 428;
+    _screenRatioHeightToDesign = _screenHeight / 926;
+    var subRatio = _screenRatioToDesign - 1;
+    _screenSemiRatioToDesign = subRatio / 2 + 1;
+    // logger.f([
+    //   "Is small screen: $_isSmallScreen ",
+    //   "Is tablet: $_isTablet ",
+    // ]);
+  }
+
+  bool _detectSmallScreen() {
+    return _screenWidth < 380;
+  }
+
+  bool _detectTablet() {
+    return _screenWidth > 600;
+  }
+
+  double _sizeScaleSmall(double ratio) => _isSmallScreen! ? ratio : 1.0;
+
+  static Future<void> ensureScreenSize([
+    ui.FlutterView? window,
+    Duration duration = const Duration(milliseconds: 10),
+  ]) async {
+    final binding = WidgetsFlutterBinding.ensureInitialized();
+    binding.deferFirstFrame();
+
+    await Future.doWhile(() {
+      window ??= binding.platformDispatcher.implicitView;
+
+      if (window == null || window!.physicalSize.isEmpty) {
+        return Future.delayed(duration, () => true);
+      }
+
+      return false;
+    });
+
+    binding.allowFirstFrame();
+  }
+}
+
+double get ratio => ScreenUtil()._screenRatioToDesign;
+
+double get screenWidth => ScreenUtil()._screenWidth;
+
+double get screenHeight => ScreenUtil()._screenHeight;
+
+abstract final class AppPlatform {
+  static bool get isTablet => ScreenUtil()._isTablet;
+
+  static bool get isSmallScreen => ScreenUtil()._isSmallScreen!;
+
+  static bool get isPhone => !ScreenUtil()._isTablet;
+}
---
+++ tester/lib/src/theme/app_size.dart
@@ -0,0 +1,17 @@
+class MyFontSize {
+  static const double label_small = 11;
+  static const double label_medium = 12;
+  static const double body_small = 12;
+  static const double title_small = 14;
+  static const double label_large = 14;
+  static const double body_medium = 14;
+  static const double title_medium = 16;
+  static const double body_large = 16;
+  static const double title_large = 22;
+  static const double headline_small = 24;
+  static const double headline_medium = 28;
+  static const double headline_large = 32;
+  static const double display_small = 36;
+  static const double display_medium = 45;
+  static const double display_large = 57;
+}
